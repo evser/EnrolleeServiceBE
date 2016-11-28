ddc5071d30b37b7e8a11b627f72ed1924ee6c453
Ratkevich, Yauheniy <Yauheniy.Ratkevich@kronos.com>
2016-11-28 12:08:26 +0300

    Transfer commit.

--- src/main/java/by/bsu/rikz/repository/TestRepository.java
+++ src/main/java/by/bsu/rikz/repository/TestRepository.java
@@ -1,9 +1,16 @@
 package by.bsu.rikz.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import by.bsu.rikz.entity.Test;
 
 public interface TestRepository extends JpaRepository<Test, Long> {
 
+	// @Query("SELECT DISTINCT test FROM Test test where test.room.university.id = :id")
+	List<Test> findByRoomUniversityId(@Param("univesityId") Long univesityId);
+
+	List<Test> findByRoomUniversityIdAndSubjectId(@Param("univesityId") Long univesityId, @Param("subjectId") Long subjectId);
 }
