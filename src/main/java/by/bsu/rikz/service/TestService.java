93b17cf0ee91033e997d9233d995797453c19f07
Yauheniy Ratkevich <evser11@yandex.ru>
2016-12-01 22:18:35 +0300

    ++ missed file

---
+++ src/main/java/by/bsu/rikz/service/TestService.java
@@ -0,0 +1,14 @@
+package by.bsu.rikz.service;
+
+import java.util.List;
+
+import org.springframework.security.core.Authentication;
+
+import by.bsu.rikz.bean.TestResultContext;
+
+public interface TestService {
+
+	public void assignTest(Authentication authentication, Long testId);
+
+	public void addTestResults(List<TestResultContext> testResults);
+}
